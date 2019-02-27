00cbc62ccd803420770f95fc74d5c24071a866b5
Lars <larsmackenzie@gmail.com>
2019-02-27 17:39:21 -0600

    storing data in json and json lines files

--- scrapyTutorial/spiders/quotes_spider.py
+++ scrapyTutorial/spiders/quotes_spider.py
@@ -1,21 +1,35 @@
 import scrapy
 
+# class QuotesSpider(scrapy.Spider):
+#     name = "quotes"
+
+#     def start_requests(self):
+#         urls = [
+#             'http://quotes.toscrape.com/page/1/',
+#             'http://quotes.toscrape.com/page/2/',
+#             'http://quotes.toscrape.com/page/3/',
+#         ]
+#         for url in urls:
+#             yield scrapy.Request(url=url, callback=self.parse)
+    
+#     def parse(self, response):
+#         page = response.url.split("/")[-2]
+#         filename = 'quotes-%s.html' % page
+#         with open(filename, 'wb') as f:
+#             f.write(response.body)
+#         self.log('Saved file %s' % filename)
+
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
 
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-            'http://quotes.toscrape.com/page/3/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-    
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
